a63af526759d415dbfe29deed46e21e0f1287482
Andrey Isambaev <isandrey2008@yandex.ru>
2025-07-16 16:14:20 +0400

    update serializers and viewsets

--- logistic/serializers.py
+++ logistic/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import  Product, Stock, StockProduct
+from django.core.exceptions import ObjectDoesNotExist
 
 
 class ProductSerializer(serializers.ModelSerializer):
@@ -14,6 +15,7 @@ class ProductPositionSerializer(serializers.ModelSerializer):
     class Meta:
         model = StockProduct
         fields = '__all__'
+        write_only_fields = ['stock']
 
 
 class StockSerializer(serializers.ModelSerializer):
@@ -32,6 +34,12 @@ class StockSerializer(serializers.ModelSerializer):
         # в нашем случае: таблицу StockProduct
         # с помощью списка positions
 
+        for position in positions:
+            position['stock'] = stock.id
+            serializer = ProductPositionSerializer(data=position)
+            if serializer.is_valid():
+                serializer.create(validated_data=serializer.validated_data)
+
         return stock
 
     def update(self, instance, validated_data):
@@ -45,6 +53,21 @@ class StockSerializer(serializers.ModelSerializer):
         # в нашем случае: таблицу StockProduct
         # с помощью списка positions
 
+        for position in positions:
+            position['stock'] = stock.id
+            serializer = ProductPositionSerializer(data=position)
+            if serializer.is_valid(raise_exception=True):
+                try:
+                    position_instance = StockProduct.objects.get(
+                        stock=serializer.validated_data['stock'],
+                        product=serializer.validated_data['product']
+                    )
+                    serializer.update(
+                        instance=position_instance,
+                        validated_data=serializer.validated_data
+                    )
+                except ObjectDoesNotExist:
+                    serializer.create(validated_data=serializer.validated_data)
         return stock
 
     class Meta:
--- logistic/views.py
+++ logistic/views.py
@@ -1,16 +1,25 @@
-from rest_framework.viewsets import ModelViewSet
+from rest_framework import viewsets, mixins
 
 from logistic.models import Product, Stock
 from logistic.serializers import ProductSerializer, StockSerializer
 
 
-class ProductViewSet(ModelViewSet):
+class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    http_method_names = ['get', 'post', 'patch', 'delete']
     # при необходимости добавьте параметры фильтрации
 
 
-class StockViewSet(ModelViewSet):
+class StockViewSet(
+    mixins.CreateModelMixin,
+    mixins.UpdateModelMixin,
+    mixins.DestroyModelMixin,
+    mixins.ListModelMixin,
+    viewsets.GenericViewSet
+):
     queryset = Stock.objects.all()
     serializer_class = StockSerializer
+    http_method_names = ['get', 'post', 'patch', 'delete']
+
     # при необходимости добавьте параметры фильтрации
